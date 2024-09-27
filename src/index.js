22956dd38d60dc11906cb0a64678b29f000d36f6
kevin fajardo <kevinfajardo1103@gmail.com>
2024-09-27 14:38:01 -0400

    grouping js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const baseUrl = "http://localhost:3000"
+const inventory = document.getElementById("featured-inventory")
+const controls = document.getElementById("controls")
+const carDetails = document.getElementById("car-details")
+const vehiclePrice = document.getElementsByClassName("vehicle-price")
+
